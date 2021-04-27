beec4e933e13a2e8285abc22b9f31aaa733500bf
Oludamilola Osiname <osinameoludamilola@gmail.com>
2021-04-27 11:31:21 +0200

    fix ssl 2

--- db/models/index.js
+++ db/models/index.js
@@ -24,7 +24,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
     dialectOptions: {
       ssl: {
-        require: true,
+        // require: true,
         rejectUnauthorized: false,
       },
     },
